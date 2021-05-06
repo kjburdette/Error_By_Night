7230af680d2ba45710ddab044dfe9f8310a8ca5d
Kendall Burdette <kjburdette2015@gmail.com>
2021-05-06 12:33:57 -0400

    seperated pages and cleaned up code

--- script.js
+++ script.js
@@ -23,48 +23,48 @@ const circle = document.querySelector('.circle');
 /**************************************************************************************************/
 /**************************************************************************************************/
 
-// Main page
-const goToLoginPage = () => {
-    mainPage.style.display = 'none';
-    loginPage.style.display = 'grid';
-};
+// // Main page
+// const goToLoginPage = () => {
+//     mainPage.style.display = 'none';
+//     loginPage.style.display = 'grid';
+// };
 
 
-middleContent.addEventListener('click', (e) => {
-    if(e.target.classList[1] === 'main-btn') {
-        goToLoginPage();
-    }
-});
+// middleContent.addEventListener('click', (e) => {
+//     if(e.target.classList[1] === 'main-btn') {
+//         goToLoginPage();
+//     }
+// });
 
-btnTop.addEventListener('click', () => {
-    const inputUserInfo = document.querySelector('.user-info');
-    const inputPassword = document.querySelector('.password');
+// btnTop.addEventListener('click', () => {
+//     const inputUserInfo = document.querySelector('.user-info');
+//     const inputPassword = document.querySelector('.password');
 
-    if(inputUserInfo.value !== "" && inputPassword.value !== "") {
-        mainPage.style.display = 'none';
-        newsFeedPage.style.display = 'block';
-    } else {
-        goToLoginPage();
-        loginModal.style.display = 'block';
-    }
-});
+//     if(inputUserInfo.value !== "" && inputPassword.value !== "") {
+//         mainPage.style.display = 'none';
+//         newsFeedPage.style.display = 'block';
+//     } else {
+//         goToLoginPage();
+//         loginModal.style.display = 'block';
+//     }
+// });
 
-// Login main
-modalX.addEventListener('click', () => {
-    loginModal.style.display = 'none';
-})
+// // Login main
+// modalX.addEventListener('click', () => {
+//     loginModal.style.display = 'none';
+// })
 
-loginFormBtn.addEventListener('click', () => {
-    const loginUserInfo = document.querySelector('.login-user-info');
-    const loginPassword = document.querySelector('.login-password');
+// loginFormBtn.addEventListener('click', () => {
+//     const loginUserInfo = document.querySelector('.login-user-info');
+//     const loginPassword = document.querySelector('.login-password');
 
-    if(loginUserInfo.value !== "" && loginPassword.value !== "") {
-        loginPage.style.display = 'none';
-        newsFeedPage.style.display = 'block';
-    } else {
-        loginModal.style.display = 'block';
-    }
-});
+//     if(loginUserInfo.value !== "" && loginPassword.value !== "") {
+//         loginPage.style.display = 'none';
+//         newsFeedPage.style.display = 'block';
+//     } else {
+//         loginModal.style.display = 'block';
+//     }
+// });
 
 // Newsfeed page
 // Post modal
